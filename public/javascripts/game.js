58e83890304577f145c6316c0fbbce958274087e
Trufa <trufavarela@gmail.com>
2016-07-11 02:37:36 -0400

    Removed client side dependencies, now goal lives on the server only

--- public/javascripts/game.js
+++ public/javascripts/game.js
@@ -1,4 +1,5 @@
 var socket = io.connect('http://localhost:3000');
+
 var s = {
     char: {
         w: 20,
@@ -7,46 +8,16 @@ var s = {
         startY: 50,
         speed: 200
     },
-    goal: {
-        w: 20,
-        h: 20
-    },
-    game: {
-        radius: 80
-    },
     uuid: uuid.v4(),
     debug: false
 };
 
 Crafty.init(document.getElementById('game'));
-Crafty.viewport.bounds = {min:{x:0, y:0}, max:{x:500, y:500}};
-
-var getRevealedDistance = function() {
-    if (!char) {
-        return "?";
-    }
-    var distance = Crafty.math.distance(
-        char.x - (char.w / 2),
-        char.y - (char.h / 2),
-        goal.x - (goal.w / 2),
-        goal.y - (goal.h / 2));
-    return parseInt(distance / s.game.radius);
-};
-
-var getRandomPosition = function() {
-    var randX = Math.floor(Math.random() * Math.floor(window.innerWidth)) + 1;
-    var randY = Math.floor(Math.random() * Math.floor(window.innerHeight)) + 1;
-    return {
-        x: randX,
-        y: randY
-    };
-};
 
 var revealedDistance = Crafty.e("2D, DOM, Text")
     .attr({ x: window.innerWidth / 2, y: 0 })
-    .text(getRevealedDistance())
+    .text("?")
     .textFont({ size: '50px', weight: 'bold' });
-
 
 var foundGoalText = Crafty.e("2D, DOM, Text")
     .attr({ x: (window.innerWidth / 2) - 100, y: 60, w: 200 })
@@ -63,22 +34,15 @@ var foundGoalText = Crafty.e("2D, DOM, Text")
         }
     });
 
-var foundGoal = function() {
-    foundGoalText.toggle();
-    char.toggle();
-    goal.setInRandomPosition();
-};
-
 var char = Crafty.e('2D, DOM, Color, Fourway, Solid')
     .attr({x: s.char.startX, y: s.char.startY, w: s.char.w, h: s.char.h})
     .color('#F00')
     .fourway(s.char.speed)
     .bind("Move", function(direction) {
-        revealedDistance.text(getRevealedDistance());
         socket.emit('position', {
             uuid: s.uuid,
-            x: 5,
-            y: 10
+            x: char.x - (char.w / 2),
+            y: char.y - (char.h / 2)
         });
     })
     .extend({
@@ -92,35 +56,23 @@ var char = Crafty.e('2D, DOM, Color, Fourway, Solid')
         }
     });
 
-var goal = Crafty.e('2D, DOM, Color, Collision')
-    .attr(getRandomPosition())
-    .attr({w: s.goal.w, h: s.goal.h})
-    .attr(function() {
-        return s.debug ? {alpha: 1} : {alpha: 0};
-    }())
-    //.attr({alpha: 0})
-    .color('gray')
-    .checkHits('Solid') // check for collisions with entities that have the Solid component in each frame
-    .bind("HitOn", function(hitData) {
-        foundGoal();
-    })
-    .extend({
-        setInRandomPosition: function() {
-            this.attr(getRandomPosition());
-        }
-    });
-
-
-
 socket.on('connect', function () {
     socket.emit('join', {
-        uuid: s.uuid
+        uuid: s.uuid,
+        windowProps: {
+            windowWidth: window.innerWidth,
+            windowHeight: window.innerHeight
+        }
     });
 });
 
-socket.on('distance', function (data) {
-    console.log(data);
-    console.log(".......");
+socket.on('distance', function (ditance) {
+    revealedDistance.text(ditance);
+});
+
+socket.on('foundGoal', function () {
+    foundGoalText.toggle();
+    char.toggle();
 });
 
 
